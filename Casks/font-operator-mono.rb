07da13dd32a6c43e5cbd7c8b35b794a98c8896cd
zacs <zac.schellhardt@gmail.com>
2020-07-01 16:56:54 -0700

    add fonts

---
+++ Casks/font-operator-mono.rb
@@ -0,0 +1,19 @@
+cask 'font-operator-mono' do
+    version :latest
+    sha256 :no_check
+  
+    url "https://raw.githubusercontent.com/zacs/homebrew-personal/master/storage/font-operator-mono/OperatorMono.zip"
+    name 'Operator Mono'
+    homepage 'https://github.com/zacs/homebrew-personal'
+  
+    font 'OperatorMono-Bold.otf'
+    font 'OperatorMono-BoldItalic.otf'
+    font 'OperatorMono-Book.otf'
+    font 'OperatorMono-BookItalic.otf'
+    font 'OperatorMono-Light.otf'
+    font 'OperatorMono-LightItalic.otf'
+    font 'OperatorMono-Medium.otf'
+    font 'OperatorMono-MediumItalic.otf'
+    font 'OperatorMono-XLight.otf'
+    font 'OperatorMono-XLightItalic.otf'
+  end
